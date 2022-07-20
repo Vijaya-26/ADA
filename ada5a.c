c145c17a51771cd08fdae7949d1ab42633f284b7
Vijaya Verma <83897655+Vijaya-26@users.noreply.github.com>
2022-07-20 10:01:01 +0530

    Update ada5a.c

--- ada5a.c
+++ ada5a.c
@@ -1,1 +1,52 @@
+//Write a prgm to print all tyhe nodes reachable from a starting node in a diagrapm using BFS method
 
+#include<stdio.h>
+#include<conio.h>
+int a[10][10],n,vis[10];
+int dfs(int);
+void main()
+{
+int i,j,src,ans;
+for(j=1;j<=n;j++)
+{
+    vis[j]=0;
+}
+printf("\nenter the no of nodes:\t");
+scanf("%d",&n);
+printf("\nenter the adjacency matrix:\n ");
+for(i=1;i<=n;i++)
+{
+ for(j=1;j<=n;j++)
+ {
+  scanf("%d",&a[i][j]);
+ }
+}
+
+printf("\nenter the source node:\t");
+scanf("%d",&src);
+ans=dfs(src);
+if(ans==1){
+     printf("graph is connected");
+}
+else{
+    printf("graph is not connected");
+}
+getch();
+}
+int dfs(int src)
+{
+    int j;
+    vis[src]=1;
+    for(j=1;j<=n;j++){
+        if(a[src][j]==1 && vis[j]!=1){
+            dfs(j);
+        }
+    }
+    for(j=1;j<=n;j++)
+    {
+        if(vis[j]!=1){
+            return 0;
+        }
+    }
+    return 1;
+}
